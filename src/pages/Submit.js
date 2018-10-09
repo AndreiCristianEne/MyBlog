8b6f369d5ce4ad7edd52503e24760618fadd2539
Andrei997 <au.andrei.ungureanu@gmail.com>
2018-10-09 22:19:16 +0200

    demonstrates how to export the article data

--- src/pages/Submit.js
+++ src/pages/Submit.js
@@ -1,12 +1,49 @@
 import React, {Component} from 'react';
 import Editor from '../components/Editor';
+import {convertToRaw} from 'draft-js'
 
-export default class Submit extends Component{
 
-    render () {
+export default class Submit extends Component {
+
+    state = {
+        title: ''
+    };
+
+    async saveArticle() {
+        const editorState = this.editor.state.editorState;
+        console.log(JSON.stringify(convertToRaw(editorState.getCurrentContent())));
+    }
+
+    render() {
         return (
             <div className="section">
-                <Editor/>
+                <div className="columns">
+                    <div className="column is-4 is-offset-4">
+                        <div className="field">
+                            <div className="control">
+                                <input className="input" placeholder="Article Title"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="columns">
+                    <div className="column is-6 is-offset-3">
+                        <Editor ref={editor => this.editor = editor}/>
+                    </div>
+                </div>
+                <div className="columns">
+                    <div className="column is-6 is-offset-3">
+                        <div className="field is-grouped">
+                            <div className="control is-expanded">
+                                <button className="button is-fullwidth" onClick={() => this.saveArticle()}>SUBMIT
+                                </button>
+                            </div>
+                            <div className="control is-expanded">
+                                <button className="button is-fullwidth" disabled={true}>PREVIEW</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
